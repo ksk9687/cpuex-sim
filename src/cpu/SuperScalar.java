e907d3d844fb1649e50e828ccdb85007a1dbf510
wata <none@none>
2009-10-28 21:19:49 +0900

    オペコードを修正

--- src/cpu/SuperScalar.java
+++ src/cpu/SuperScalar.java
@@ -22,63 +22,63 @@ public class SuperScalar extends CPU {
 	
 	protected int getBinary(String op, Parser p) {
 		if (op.equals("li")) {
-			return typeI(0, 0, reg(p), imm(p, 14, false));
+			return typeI(000, 0, reg(p), imm(p, 14, false));
 		} else if (op.equals("add")) {
-			return typeR(1, reg(p), reg(p), reg(p));
+			return typeR(010, reg(p), reg(p), reg(p));
 		} else if (op.equals("addi")) {
-			return typeI(2, reg(p), reg(p), imm(p, 14, true));
+			return typeI(001, reg(p), reg(p), imm(p, 14, true));
 		} else if (op.equals("sub")) {
-			return typeR(3, reg(p), reg(p), reg(p));
+			return typeR(011, reg(p), reg(p), reg(p));
 		} else if (op.equals("sll")) {
-			return typeI(4, reg(p), reg(p), imm(p, 5, false));
+			return typeI(002, reg(p), reg(p), imm(p, 5, false));
 		} else if (op.equals("cmp")) {
-			return typeI(5, reg(p), reg(p), 0);
+			return typeI(012, reg(p), reg(p), 0);
 		} else if (op.equals("cmpi")) {
-			return typeI(6, reg(p), 0, imm(p, 14, true));
+			return typeI(003, reg(p), 0, imm(p, 14, true));
 		} else if (op.equals("fadd")) {
-			return typeR(7, reg(p), reg(p), reg(p));
+			return typeR(020, reg(p), reg(p), reg(p));
 		} else if (op.equals("fsub")) {
-			return typeR(8, reg(p), reg(p), reg(p));
+			return typeR(021, reg(p), reg(p), reg(p));
 		} else if (op.equals("fmul")) {
-			return typeR(9, reg(p), reg(p), reg(p));
+			return typeR(022, reg(p), reg(p), reg(p));
 		} else if (op.equals("finv")) {
-			return typeR(10, reg(p), 0, reg(p));
+			return typeR(023, reg(p), 0, reg(p));
 		} else if (op.equals("fsqrt")) {
-			return typeR(11, reg(p), 0, reg(p));
+			return typeR(024, reg(p), 0, reg(p));
 		} else if (op.equals("fcmp")) {
-			return typeI(12, reg(p), reg(p), 0);
+			return typeI(025, reg(p), reg(p), 0);
 		} else if (op.equals("fabs")) {
-			return typeR(13, reg(p), 0, reg(p));
+			return typeR(026, reg(p), 0, reg(p));
 		} else if (op.equals("fneg")) {
-			return typeR(14, reg(p), 0, reg(p));
+			return typeR(027, reg(p), 0, reg(p));
 		} else if (op.equals("load")) {
-			return typeI(15, reg(p), reg(p), imm(p, 14, true));
+			return typeI(030, reg(p), reg(p), imm(p, 14, true));
 		} else if (op.equals("loadr")) {
-			return typeI(16, reg(p), reg(p), reg(p));
+			return typeI(031, reg(p), reg(p), reg(p));
 		} else if (op.equals("store")) {
-			return typeI(17, reg(p), reg(p), imm(p, 14, true));
+			return typeI(032, reg(p), reg(p), imm(p, 14, true));
 		} else if (op.equals("hsread")) {
 			throw new AssembleException("Not implemented");
 		} else if (op.equals("hswrite")) {
 			throw new AssembleException("Not implemented");
 		} else if (op.equals("read")) {
-			return typeI(20, 0, reg(p), 0);
+			return typeI(050, 0, reg(p), 0);
 		} else if (op.equals("write")) {
-			return typeI(21, reg(p), 0, 0);
+			return typeI(051, reg(p), 0, 0);
 		} else if (op.equals("ledout")) {
-			return typeI(22, reg(p), 0, 0);
+			return typeI(052, reg(p), 0, 0);
 		} else if (op.equals("nop")) {
-			return typeI(23, 0, 0, 0);
+			return typeI(060, 0, 0, 0);
 		} else if (op.equals("halt")) {
-			return typeI(24, 0, 0, 0);
+			return typeI(061, 0, 0, 0);
 		} else if (op.equals("mov")) {
-			return typeI(25, reg(p), reg(p), 0);
+			return typeI(047, reg(p), reg(p), 0);
 		} else if (op.equals("jmp")) {
-			return typeI(26, imm(p, 3, false), 0, imm(p, 14, false));
+			return typeI(070, imm(p, 3, false), 0, imm(p, 14, false));
 		} else if (op.equals("jal")) {
-			return typeI(27, reg(p), 0, 0);
+			return typeI(071, reg(p), 0, 0);
 		} else if (op.equals("jr")) {
-			return typeI(28, 0, 0, 0);
+			return typeI(072, 0, 0, 0);
 		}
 		return super.getBinary(op, p);
 	}
@@ -112,90 +112,90 @@ public class SuperScalar extends CPU {
 	}
 	
 	protected void step(int ope, int opecode, int rs, int rt, int rd, int imm, int addr) {
-		if (opecode == 0) { //li
+		if (opecode == 000) { //li
 			regs[rd] = imm;
 			pc++;
-		} else if (opecode == 1) { //add
+		} else if (opecode == 010) { //add
 			regs[rd] = regs[rs] + regs[rt];
 			pc++;
-		} else if (opecode == 2) { //addi
+		} else if (opecode == 001) { //addi
 			regs[rt] = regs[rs] + signExt(imm, 14);
 			pc++;
-		} else if (opecode == 3) { //sub
+		} else if (opecode == 011) { //sub
 			regs[rd] = regs[rs] - regs[rt];
 			pc++;
-		} else if (opecode == 4) { //sll
+		} else if (opecode == 002) { //sll
 			regs[rt] = regs[rs] << imm;
 			pc++;
-		} else if (opecode == 5) { //cmp
+		} else if (opecode == 012) { //cmp
 			cond = cmp(regs[rs], regs[rt]);
 			pc++;
-		} else if (opecode == 6) { //cmpi
+		} else if (opecode == 003) { //cmpi
 			cond = cmp(regs[rs], imm);
 			pc++;
-		} else if (opecode == 7) { //fadd
+		} else if (opecode == 020) { //fadd
 			regs[rd] = ftoi(itof(regs[rs]) + itof(regs[rt]));
 			pc++;
-		} else if (opecode == 8) { //fsub
+		} else if (opecode == 021) { //fsub
 			regs[rd] = ftoi(itof(regs[rs]) - itof(regs[rt]));
 			pc++;
-		} else if (opecode == 9) { //fmul
+		} else if (opecode == 022) { //fmul
 			regs[rd] = ftoi(itof(regs[rs]) * itof(regs[rt]));
 			pc++;
-		} else if (opecode == 10) { //finv
+		} else if (opecode == 023) { //finv
 			regs[rd] = ftoi(1.0f / itof(regs[rs]));
 			pc++;
-		} else if (opecode == 11) { //fsqrt
+		} else if (opecode == 024) { //fsqrt
 			regs[rd] = ftoi((float)sqrt(itof(regs[rs])));
 			pc++;
-		} else if (opecode == 12) { //fcmp
+		} else if (opecode == 025) { //fcmp
 			cond = fcmp(regs[rs], regs[rt]);
 			pc++;
-		} else if (opecode == 13) { //fabs
+		} else if (opecode == 026) { //fabs
 			regs[rd] = ftoi(abs(itof(regs[rs])));
 			pc++;
-		} else if (opecode == 14) { //fneg
+		} else if (opecode == 027) { //fneg
 			regs[rd] = ftoi(-(itof(regs[rs])));
 			pc++;
-		} else if (opecode == 15) { //load
+		} else if (opecode == 030) { //load
 			regs[rt] = load(regs[rs] + signExt(imm, 14));
 			pc++;
-		} else if (opecode == 16) { //loadr
+		} else if (opecode == 031) { //loadr
 			regs[rd] = load(regs[rs] + regs[rt]);
 			pc++;
-		} else if (opecode == 17) { //store
+		} else if (opecode == 032) { //store
 			store(regs[rs] + signExt(imm, 14), regs[rt]);
 			pc++;
-		} else if (opecode == 18) { //hsread
+		} else if (opecode == 040) { //hsread
 			throw new ExecuteException("Not implemented");
-		} else if (opecode == 19) { //hswrite
+		} else if (opecode == 041) { //hswrite
 			throw new ExecuteException("Not implemented");
-		} else if (opecode == 20) { //read
+		} else if (opecode == 050) { //read
 			regs[rt] = read();
 			pc++;
-		} else if (opecode == 21) { //write
+		} else if (opecode == 051) { //write
 			cond = write(regs[rs]);
 			pc++;
-		} else if (opecode == 22) { //ledout
+		} else if (opecode == 052) { //ledout
 			System.err.printf("LED: %s%n", toBinary(regs[rs]).substring(24));
 			pc++;
-		} else if (opecode == 23) { //nop
+		} else if (opecode == 060) { //nop
 			pc++;
-		} else if (opecode == 24) { //halt
+		} else if (opecode == 061) { //halt
 			halted = true;
-		} else if (opecode == 25) { //mov
+		} else if (opecode == 047) { //mov
 			regs[rt] = regs[rs];
 			pc++;
-		} else if (opecode == 26) { //jmp
+		} else if (opecode == 070) { //jmp
 			if ((cond & rs) == 0) {
 				pc = imm;
 			} else {
 				pc++;
 			}
-		} else if (opecode == 27) { //jal
+		} else if (opecode == 071) { //jal
 			regs[63] = pc + 1;
 			pc = imm;
-		} else if (opecode == 28) { //jr
+		} else if (opecode == 072) { //jr
 			pc = regs[rs];
 		} else {
 			super.step(ope);
